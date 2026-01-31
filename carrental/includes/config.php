0423f2ac2dd0e85aaa703e7c4456a4c53bed0a92
Aditya981920 <aditya981920@gmail.com>
2026-01-31 14:29:56 +0530

    Update DB config to use environment variables

--- carrental/includes/config.php
+++ carrental/includes/config.php
@@ -1,16 +1,18 @@
-<?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','carrental');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+<?php
+define('DB_HOST', getenv("DB_HOST"));
+define('DB_USER', getenv("DB_USER"));
+define('DB_PASS', getenv("DB_PASS"));
+define('DB_NAME', getenv("DB_NAME"));
+define('DB_PORT', getenv("DB_PORT") ?: 3306);
+
+try {
+    $dbh = new PDO(
+        "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME,
+        DB_USER,
+        DB_PASS,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+    );
+} catch (PDOException $e) {
+    exit("DB Error: " . $e->getMessage());
 }
 ?>
